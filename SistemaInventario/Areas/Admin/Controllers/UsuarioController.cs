2007b61508a2323bdc536f0ef7bc152657cf0e01
Xavier Valles <xvv1988@gmail.com>
2024-02-07 09:03:08 +0100

    Administracion de usuarios

---
+++ SistemaInventario/Areas/Admin/Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        private readonly ApplicationDbContext _db;
+        public UsuarioController(IUnidadTrabajo unidadTrabajo, ApplicationDbContext db)
+        {
+            _unidadTrabajo = unidadTrabajo;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var usuarioLista = await _unidadTrabajo.UsuarioAplicacion.ObtenerTodos();
+            var userRole = await _db.UserRoles.ToListAsync();
+            var roles = await _db.Roles.ToListAsync();
+
+            foreach (var usuario in usuarioLista)
+            {
+                var roleId = userRole.FirstOrDefault(x => x.UserId == usuario.Id).RoleId;
+                usuario.Role = roles.FirstOrDefault(x => x.Id == roleId).Name;
+            }
+            return Json(new { data = usuarioLista });
+
+        }
+
+        public async Task<IActionResult> BloquearDesbloquear([FromBody] string id)
+        {
+            var usuario = await _unidadTrabajo.UsuarioAplicacion.ObtenerPrimero(x => x.Id == id);
+            if (usuario == null)
+            {
+                return Json(new { success = false, message = "Error de usuario" });
+            }
+            if (usuario.LockoutEnd != null && usuario.LockoutEnd > DateTime.Now)
+            {
+                usuario.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                usuario.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Operacion realizada con exitus." });
+        }
+
+        #endregion
+    }
+}
